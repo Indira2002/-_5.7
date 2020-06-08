0eec064f5ed59a933728c082ed4f282720bc2f5a
indira <indira.sultanova.2002@mail.ru>
2020-06-08 15:53:00 +0500

    commit osnovi

---
+++ osnovi/osnovi.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+
+{
+	const int m = 10, n = 10;
+
+	int matr[m][n];
+
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			matr[i][j] = i * n + j;
+			cout << matr[i][j] << " ";
+		}
+	cout << endl;
+	
+}
+
